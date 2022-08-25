4721c7e9fc33861254cd29f778a131b53e74874f
Ann Oreng <orengann@gmail.com>
2022-08-25 10:48:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function updateEmployeeWithKeyAndValue(employee, key, value){
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
     employee[key] = value;
-
     return employee;
 }
 
